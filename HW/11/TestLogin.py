83d198c640354e9d320556974b1d18904f347859
pif34 <pif345678@gmail.com>
2023-12-15 17:05:44 +0400

    Lesson 12

--- HW/11/TestLogin.py
+++ HW/11/TestLogin.py
@@ -7,19 +7,19 @@ link = "http://34.141.58.52:8080/#/login"
 
 class TestLogin:
 
-    @pytest.mark.smoke
+    # @pytest.mark.smoke
     def test_input_login(self, browser):
         browser.get(link)
         input1 = browser.find_element(By.ID, "login")
         input1.send_keys(login)
 
-    @pytest.mark.smoke
+    # @pytest.mark.skip
     def test_input_password(self, browser):
         browser.get(link)
         input2 = browser.find_element(By.CSS_SELECTOR, "#password > input")
         input2.send_keys(password)
 
-    @pytest.mark.regress
+    # @pytest.mark.regress
     def test_submit_button(self, browser):
         browser.get(link)
         button = browser.find_element(By.CLASS_NAME, "p-button-label")
